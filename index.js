f851d980323f5948d7bde92814e3f50ee5d13528
mihirrd <deshpande.mihir7@gmail.com>
2021-01-22 13:27:10 +0530

    connection to db successful solved timeout issue separated the db connect logic from index.js to new db.js file

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import express from 'express' //for creating server and routes
-import mongoose from 'mongoose' //for connecting to mongodb
 import dotenv from 'dotenv'  //for environment variables
+import connectdb from './db.js'
 import userRoutes from './routes/users.js'
 
+
 //setting up the server
-dotenv.config()
+dotenv.config({path : './config/config.env'})
 const server = express()
 const PORT = process.env.port || 5000
 
 //connecting to the database
-try {
-    mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-    console.log("connected"));    
-}catch (error) { 
-    console.log("could not connect" + error);    
-}
+connectdb()
 
 server.use('/users', userRoutes)
 
